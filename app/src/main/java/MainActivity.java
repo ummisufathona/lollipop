f4406c84bb934f96bc4d57b966aeb1c6f2369a53
ummisufathona <ummisufathona13@gmail.com>
2020-03-20 17:37:22 +0700

    Initial commit

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.teknokrat.mobile2019.ti17a17313044.lollipop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/teknokrat/mobile2019/ti17a17313044/lollipop/MainActivity.java
@@ -0,0 +1,14 @@
+package com.teknokrat.mobile2019.ti17a17313044.lollipop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
